424940c708a1e066173a99084c0bdc87eb6af23f
Soheil Changizi <cocolico12@gmail.com>
2019-10-19 20:51:21 +0330

    Update train.py

--- train.py
+++ train.py
@@ -9,18 +9,20 @@ from agent import Agent
 def preprocess(frame):
     # Got some ideas from https://github.com/ageron/tiny-dqn
     mspacman_color = np.array([210, 164, 74]).mean()
+    kernel = np.ones((3,3),np.uint8)
+    dilation = cv2.dilate(frame,kernel,iterations = 2)
     img = frame[1:176:2,::2]
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img[img==mspacman_color] = 0 
     img = cv2.normalize(img, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
 
-    return np.expand_dims(img.reshape(88, 80, 1), axis=0)
+    return img.reshape(88, 80, 1)
 
 
 env = gym.make('MsPacman-v4') # Skip 4 Frames
 state_size = (88, 80, 1)
-action_size = env.action_space.n
-agent = Agent(state_size, action_size, 4) # 4-step return
+action_size = 5
+agent = Agent(state_size, action_size, 5) # 5-step return
 
 episodes = 20000
 batch_size = 32
@@ -31,7 +33,7 @@ done = False
 # Initializing Buffer
 while len(agent.buffer) < 10000:
   state = preprocess(env.reset())
-  frame_stack = deque(maxlen=3) # Deque for getting mean of 3 frames instead of stacking
+  frame_stack = deque(maxlen=4) # Deque for getting mean of 4 frames instead of stacking
   frame_stack.append(state)
 
   for skip in range(90): # Skip first 3 seconds of the game
@@ -40,8 +42,8 @@ while len(agent.buffer) < 10000:
   for time in range(10000):
 
       state = sum(frame_stack)/len(frame_stack)
-
-      action = agent.act(state)
+  
+      action = agent.act(np.expand_dims(state.reshape(88, 80, 1), axis=0))
       next_state, reward, done, _ = env.step(action)
 
       next_state = preprocess(next_state)
@@ -63,7 +65,7 @@ for e in tqdm(range(0, episodes)):
     total_reward = 0
     game_score = 0
     state = preprocess(env.reset())
-    frame_stack = deque(maxlen=3)
+    frame_stack = deque(maxlen=4)
     frame_stack.append(state)
     
     for skip in range(90):
@@ -77,7 +79,7 @@ for e in tqdm(range(0, episodes)):
         
         state = sum(frame_stack)/len(frame_stack)
         
-        action = agent.act(state)
+        action = agent.act(np.expand_dims(state.reshape(88, 80, 1), axis=0))
         next_state, reward, done, _ = env.step(action)
         
         next_state = preprocess(next_state)
