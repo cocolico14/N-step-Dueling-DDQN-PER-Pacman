98d5c625daab19c8e1da7cff9c4d50b0877b4b80
Soheil Changizi <cocolico12@gmail.com>
2019-10-27 22:48:30 +0330

    Update test.py

--- test.py
+++ test.py
@@ -12,21 +12,23 @@ def wrap_env(env):
 def preprocess(frame):
     # Got some ideas from https://github.com/ageron/tiny-dqn
     mspacman_color = np.array([210, 164, 74]).mean()
+    kernel = np.ones((3,3),np.uint8)
+    dilation = cv2.dilate(frame,kernel,iterations = 2)
     img = frame[1:176:2,::2]
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img[img==mspacman_color] = 0 
     img = cv2.normalize(img, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
 
-    return np.expand_dims(img.reshape(88, 80, 1), axis=0)
+    return img.reshape(88, 80, 1)
 
 env = wrap_env(gym.make("MsPacman-v4"))
 state_size = (88, 80, 1)
-action_size = env.action_space.n
-agent = Agent(state_size, action_size, 4)
+action_size = 5
+agent = Agent(state_size, action_size, 5)
 # agent.load("")
 
 state = preprocess(env.reset())
-frame_stack = deque(maxlen=3)
+frame_stack = deque(maxlen=4)
 frame_stack.append(state)
 
 for skip in range(90):
@@ -38,7 +40,7 @@ while True:
     env.render()
     state = sum(frame_stack)/len(frame_stack)
     
-    action = agent.act(state)
+    action = agent.act(np.expand_dims(state.reshape(88, 80, 1), axis=0))
     next_state, reward, done, _ = env.step(action)
     
     next_state = preprocess(next_state)
